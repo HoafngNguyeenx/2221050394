960e716365d70fb5315f75bcc6b3eb206b6fa23c
HoafngNguyeenx <hoangh212369@gmail.com>
2025-11-03 18:16:54 +0700

    Bai13

---
+++ bai13/scrip.js
@@ -0,0 +1,46 @@
+let danhSachphim = [
+    {
+        id: 1,
+        tenPhim: "Mưa đỏ"
+        namPhathanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam"
+        poster: 
+        theLoai: "Phim chiếu rạp"
+    }
+    {
+        id: 2,
+        tenPhim: "Conan"
+        namPhathanh: 2020
+        thoiLuong: 2
+        quocGia: "Nhật Bản"
+        poster: 
+        theLoai: "Phim hoạt hình"
+
+    }
+    {
+        id: 3,
+        tenPhim: "Ma Đồng Náo Hải"
+        namPhathanh: 2025
+        thoiLuong: 2
+        quocGia: "Trung Quốc"
+        poster:
+        theLoai: "Phim hoạt hình"
+        
+    }
+    
+];
+let phimHientai = danhSachphim[0];
+let banner = document.getElementsByClassName('banner')[0];
+function chonPhim(idPhim){
+    for (let i = 0; i < danhSachphim.length; i++){
+        if (danhSachphim[i].id === idPhim) {
+            banner.computedStyleMap.backgroundImage = danhSachphim[i].poster;
+
+        }
+    }
+}
+
+
+
